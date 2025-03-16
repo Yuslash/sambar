94f1f327635f43aee909b30ecf9d93f9d1842ced
Yuslash <sibikrishna71@gmail.com>
2025-03-16 23:11:00 +0530

    fixing

--- src/Components/MainPanels/Discussion.jsx
+++ src/Components/MainPanels/Discussion.jsx
@@ -2,7 +2,7 @@ export default function DiscussionPanel() {
     return (
         <div className="p-6 text-white bg-indigo-500">
             <h1 className="text-2xl font-bold">Discussion Panel</h1>
-            <p>Here you can chat and discuss topics.</p>
+            <p>Gonna add some reveal animation here.</p>
         </div>
     );
 }
